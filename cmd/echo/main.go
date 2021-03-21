18f5b196dbad14eac22ae8a25b60a0bc54ac0afa
興怡 <>
2021-03-21 04:53:12 -0700

    time/unmarshal compile err

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -5,17 +5,17 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"math/rand"
-	"net/http"
+
 	"time"
 )
 
 type rtype struct {
-	Success bool `json:"success"`
-	Data dtype `json:"data"`
+	Success bool  `json:"success"`
+	Data    dtype `json:"data"`
 }
 type dtype struct {
-	Word string `json:"word"`
-	Vector []int `json:"vector"`
+	Word   string `json:"word"`
+	Vector []int  `json:"vector"`
 }
 type qtype struct {
 	Id string `json:"id"`
@@ -31,22 +31,22 @@ func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResp
 	}
 
 	// make arbitrary JSON for answer, atm
-	rand.Seed(time.Now.Unix())
+	rand.Seed(time.Now().Unix())
 	nums := rand.Perm(4)
-	data := dtype {Word: word, Vector: nums}
-	payload := rtype {Success: true, Data: data}
-	answer,_ := json.Marshal(payload)
+	data := dtype{Word: word, Vector: nums}
+	payload := rtype{Success: true, Data: data}
+	answer, _ := json.Marshal(payload)
 
-	headers := map[string]string {
-		"Content-Type": "application/json",
+	headers := map[string]string{
+		"Content-Type":  "application/json",
 		"Cache-Control": "public, max-age=300",
 	}
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           headers,
+		StatusCode: 200,
+		Headers:    headers,
 		////MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              string(answer),
-		IsBase64Encoded:   false,
+		Body:            string(answer),
+		IsBase64Encoded: false,
 	}, nil
 }
 
@@ -60,10 +60,9 @@ func extractField(body string) (word string, err error) {
 	var qry qtype
 	word = ""
 	// expect JSON that contains a "id" field
-	err = json.UnMarshal(body, &qry)
+	err = json.Unmarshal([]byte(body), &qry)
 	if err != nil {
 		return
 	}
 	return qry.Id, nil
 }
-
